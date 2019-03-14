d779d70cc28d1638b4b142cbe0b9a88351c19574
Sage Hogue <Sagehogue@gmail.com>
2019-03-13 20:49:45 -0700

    began will bootstrap project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Jumbotron fluid>
+          <h1>Universe, meet Will Lucas.</h1>
+          <h5>I'm an adventurous Portland native and triple threat.</h5>
+        </Jumbotron>
       </div>
     );
   }
