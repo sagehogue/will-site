372e5173109e57c60b694b3855254ffdadef472b
Sage Hogue <Sagehogue@gmail.com>
2019-03-21 19:13:38 -0700

    working on sanity integration

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import classes from './Image.module.css';
 
-const Image = ({src, rounded, fluid, clickHandler, alt, id}) => {
+const Image = ({src, rounded, fluid, clickHandler, alt, id, fullsize}) => {
     let classList = [classes.Image];
     classList.push(rounded? classes.rounded : '');
     classList.push(fluid? classes.fluid : '');
+    classList.push(fullsize? classes.fullsize : '');
     return <img id={id} src={src} onClick={clickHandler} className={classList.join(' ')}alt={alt} />
 }
 
--- src/components/ImageGroup/ImageGroup.js
+++ src/components/ImageGroup/ImageGroup.js
@@ -5,7 +5,9 @@ import classes from './ImageGroup.module.css';
 
 import backdropContext from '../Backdrop/BackdropContext';
 
-// MOST RECENT: Need to get the full <Image /> in the fullsizeimage property in state.
+// MOST RECENT: I think this would be much simpler if I refactored it as a class. Hooks and context are fun and all, but this is not the ideal use case. It is growing bloated and convoluted to achieve what is simply achieved with known tools.
+
+// SECOND MOST RECENT: Need to get the full <Image /> in the fullsizeimage property in state.
 
 // I need to document this well, it's getting a bit clusterfucky. Next on the list is building gallery functionality that will allow someone to click arrows to go through the images.
 
@@ -17,12 +19,15 @@ const imageGroup = ({ imageURLs }) => {
     // Positioned first so the method is defined when state is initialized.
     const imageClickHandler = event => {
         toggle({ backdropClosed: false });
-        const idOfImg = event.target.id;
-        const newState = setState(oldState => {
+        const idOfImg = event.target.src;
+        console.log(event, idOfImg);
+        event.persist();
+        const newState = setState((oldState) => {
             return ({
-                ...oldState, displayingFullImage: event.target.id
+                ...oldState, displayingFullImage: event.target.src
             })
         })
+        console.log(state);
     }
     const [state, setState] = useState({
         displayingFullImage: false,
@@ -31,23 +36,30 @@ const imageGroup = ({ imageURLs }) => {
 
 
     const determineHowImagesRender = (imageURLs) => {
-        
         return imageURLs.map((url, index) => {
             return <Image
                 clickHandler={imageClickHandler}
                 src={url}
                 key={index}
                 id={index}
-                fullsize={state.displayingFullImage !== false && url !== state.displayingFullImage.props.src ? true : false}
+                fullsize={state.displayingFullImage === url ? true : false}
                 fluid rounded></Image>
         })
     }
 
-    let classArray = [classes.imageGroup]
+    // setState((oldState) => {
+    //     return {
+    //         ...state,
+    //         images: determineHowImagesRender(imageURLs)
+    //     }
+    // })
+
+    let classArray = [classes.imageGroup, state.displayingFullImage ? classes.fullSizeContainer : '']
     return (
         <div className={classArray.join(' ')}>
-            {determineHowImagesRender(imageURLs)}
-
+            {useEffect(() => {
+                determineHowImagesRender(imageURLs);
+            }) || determineHowImagesRender(imageURLs)}
             {/* {state.images.filter(image => {
                 // 
                 return image !== state.displayingFullImage? true : false;
