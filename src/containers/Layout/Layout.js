e27f5059f61c59a921271a1ae7c7022ba9f68c5d
Sage Hogue <Sagehogue@gmail.com>
2019-04-01 13:17:46 -0700

    made some aesthetic strides

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -26,9 +26,9 @@ import seattle from './images/seattle-1.JPG'
 import classes from './Layout.module.css';
 
 const Layout = props => {
-    const [backdropState, backdropToggle] = React.useState({backdropClosed: true})
+    const [backdropState, backdropToggle] = React.useState({ backdropClosed: true })
     const handler = () => {
-        backdropToggle({backdropClosed: true});
+        backdropToggle({ backdropClosed: true });
     }
     return (
         <>
@@ -38,48 +38,54 @@ const Layout = props => {
                     <h5>I'm an adventurous Portland native and triple threat.</h5>
                 </Jumbotron>
                 <div className={classes.wrapper}>
-                    <div className={[classes.imageWrapper, classes.offsetY, classes.grid].join(' ')}>
+                    <div className={[classes.imageWrapper, classes.center,  classes.marginBottomLarge, classes.offsetY, classes.grid].join(' ')}>
                         <Image src={arkadiusz} fluid rounded />
                         <Image src={sageSquat} fluid rounded />
-                        {/* <ImageGroup imageURLs={[arkadiusz, sageSquat]}>
-                    </ImageGroup> */}
                         <div className={[classes.caption, classes.pullDown].join(' ')}>
-                            fitness photographer
-                        <div className={classes.stripe} />
+                            <span>
+                                fitness photographer
+                                </span>
+                            <div className={classes.stripe} />
                         </div>
                     </div>
-                    <div className={classes.imageWrapper}>
+                    <div className={[classes.imageWrapper, classes.imgWrapperLandscape, classes.center].join(' ')}>
                         <Image src={willSquat} fluid rounded />
-                        <div className={[classes.caption, classes.colorPowerliftPink, classes.alignCenter, classes.pullDownLess].join(' ')}>
-                            powerlifter
-                        <div className={[classes.stripe, classes.bgBlue].join(' ')} />
+                        <div className=
+                            {[classes.caption, classes.colorPowerliftPink, classes.alignCenter, classes.pullDownLess].join(' ')}>
+                            <span>
+                                powerlifter
+                        </span>
+                            <div className={[classes.stripe, classes.bgBlue].join(' ')} />
                         </div>
 
                     </div>
-                    <div className={classes.imageWrapper}>
+                    <div className={[classes.imageWrapper, classes.imgWrapperLandscape, classes.center].join(' ')}>
                         <Image src={willFlex} fluid rounded />
-                        <div className={[classes.caption, classes.colorTeal, classes.pullDownMuchLess].join(' ')}>
-                            occasional model.
-                    </div>
-
+                        <div className={[classes.caption, classes.colorTeal, classes.pullDownOther, classes.center].join(' ')}>
+                            <span>
+                                occasional model.
+                        </span>
+                        </div>
                     </div>
                     <div className={classes.galleryPreface}>
                         <h3>Sometimes I take pictures of the things I see..</h3>
                     </div>
-                    <ImageGroup
-                        imageQuantity={"experiment"}
-                        imageURLs={[house, car, seattle, cameraSelfie, face, lake, carCloseUp, max, lake2, people]}>
-                        <Image src={house} fluid rounded />
-                        <Image src={car} fluid rounded />
-                        <Image src={seattle} fluid rounded />
-                        <Image src={cameraSelfie} fluid rounded />
-                        <Image src={face} fluid rounded />
-                        <Image src={lake} fluid rounded />
-                        <Image src={carCloseUp} fluid rounded />
-                        <Image src={max} fluid rounded />
-                        <Image src={lake2} fluid rounded />
-                        <Image src={people} fluid rounded />
-                    </ImageGroup>
+                        <div className={classes.galleryWrapper}>
+                        <ImageGroup
+                            imageQuantity={"experiment"}
+                            imageURLs={[house, car, seattle, cameraSelfie, face, lake, carCloseUp, max, lake2, people]}>
+                            <Image src={house} fluid rounded />
+                            <Image src={car} fluid rounded />
+                            <Image src={seattle} fluid rounded />
+                            <Image src={cameraSelfie} fluid rounded />
+                            <Image src={face} fluid rounded />
+                            <Image src={lake} fluid rounded />
+                            <Image src={carCloseUp} fluid rounded />
+                            <Image src={max} fluid rounded />
+                            <Image src={lake2} fluid rounded />
+                            <Image src={people} fluid rounded />
+                        </ImageGroup>
+                    </div>
                     <Backdrop isDisabled={backdropState.backdropClosed} menuHandler={handler} />
                 </div>
             </backdropContext.Provider>
